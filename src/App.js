5124fcb2c2d86b9a3b88dc3d4066586aaec4d499
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-11-03 20:55:22 +0500

    React dasturini boshlang'ich yurutib oldim.

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import { Routers } from "./shared/router";
+function App() {
+  return (
+    <div className="App">
+      <Routers />
+      <ToastContainer
+        position="top-center"
+        autoClose={2500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </div>
+  );
+}
+
+export default App;
