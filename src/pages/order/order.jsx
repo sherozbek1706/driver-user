9bc28290328c7633a54cd3b108c89c45b8dabc00
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-11-04 19:26:50 +0500

    zakazlar bilan ishlash uchun page yozildi.

---
+++ src/pages/order/order.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from "../../components";
+import "./order.css";
+
+export const Order = () => {
+  return (
+    <div className="DefaultHome">
+      <div className="Order">
+        <Navbar />
+      </div>
+    </div>
+  );
+};
