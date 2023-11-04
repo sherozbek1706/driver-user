d1c3be8b5695685bcc2373d6c22d29cb5cba8737
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-11-04 19:25:24 +0500

    main css ochdim asosiy stylar kiritib boraman.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+import "./assets/main.css";
 import { Routers } from "./shared/router";
 function App() {
   return (
