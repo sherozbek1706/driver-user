15d7f43a60b2da018e3f15496123bf6a818a15d5
sherozbek1706 <baxtiyorovsherozbek1111@gmail.com>
2023-11-10 23:31:02 +0500

    Loader yasadim, layout sifatida

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,1 +1,2 @@
 export * from "./build/build";
+export * from "./loader/loader"
---
+++ src/layouts/loader/loader.jsx
@@ -0,0 +1,8 @@
+import "./loader.css"
+export const Loader = () => {
+    return (
+        <div className="Loader page_build">
+            <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        </div>
+    )
+}
