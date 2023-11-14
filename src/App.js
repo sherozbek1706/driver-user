cced8bb3b27f321846d0004433fc6c237531fc4b
sherozbek1706 <baxtiyorovsherozbek1111@gmail.com>
2023-11-14 11:57:59 +0500

    socket ulandi

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+import io from "socket.io-client";
 import "./assets/main.css";
 import { Routers } from "./shared/router";
+import { api } from "./utils";
+
+export const socket = io.connect(api);
+
 function App() {
   return (
     <div className="App">
